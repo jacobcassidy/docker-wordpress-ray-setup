9bca6e9fe67973b93ab531e88d93d6f4f2815399
Jacob Cassidy <jacob@jacobcassidy.com>
2024-04-01 03:24:08 +0700

    Format `ray.php` with tabs instead of spaces to match WP standards

--- ray.php
+++ ray.php
@@ -5,29 +5,30 @@
  * @package Global Ray
  */
 
-return [
-    /*
-     * This settings controls whether data should be sent to Ray.
-     */
-    'enable' => true,
+return array(
 
-    /*
-     * The host used to communicate with the Ray app.
-     */
-    'host' => 'host.docker.internal',
+	/*
+	 * This settings controls whether data should be sent to Ray.
+	 */
+	'enable'      => true,
 
-    /*
-     * The port number used to communicate with the Ray app.
-     */
-    'port' => 23517,
+	/*
+	 * The host used to communicate with the Ray app.
+	 */
+	'host'        => 'host.docker.internal',
 
-    /*
-     * Absolute base path for your sites or projects in Homestead, Vagrant, Docker, or another remote development server.
-     */
-    'remote_path' => '/var/www/html',
+	/*
+	 * The port number used to communicate with the Ray app.
+	 */
+	'port'        => 23517,
 
-    /*
-     * Absolute base path for your sites or projects on your local computer where your IDE or code editor is running on.
-     */
-    'local_path' => getenv('LOCAL_PATH'),
-];
+	/*
+	 * Absolute base path for your sites or projects in Homestead, Vagrant, Docker, or another remote development server.
+	 */
+	'remote_path' => '/var/www/html',
+
+	/*
+	 * Absolute base path for your sites or projects on your local computer where your IDE or code editor is running on.
+	 */
+	'local_path'  => getenv( 'LOCAL_PATH' ),
+);
